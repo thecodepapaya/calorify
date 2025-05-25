b669ac246450dedcffad71fd766efecc4cb4a27a
ashutosh.s <ashutosh.s@even.in>
2025-05-26 00:51:03 +0400

    Fix connect button not redirecting to permission

--- android/app/src/main/kotlin/dev/thecodepapaya/calorify/MainActivity.kt
+++ android/app/src/main/kotlin/dev/thecodepapaya/calorify/MainActivity.kt
@@ -1,5 +1,5 @@
 package dev.thecodepapaya.calorify
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
